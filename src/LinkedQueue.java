9e2f0416c342500dd4613071a41027bc204b202f
Ryan <rsgm123@gmail.com>
2015-04-24 23:24:10 -0500

    Formatting

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,21 +1,21 @@
 
-public class LinkedQueue<T> implements Queue<T>{
+public class LinkedQueue<T> implements Queue<T> {
     private Node head = null;
     private int count = 0;
 
     /**
      * Offer the item into this Queue at the rear end.
+     *
      * @param item
      * @return false if fail to do so, in case the capacity is restricted.
      */
-    public void offer(T item){
+    public void offer(T item) {
         if (size() == 0) {
             head = new Node(head, item);
-        }
-        else {
+        } else {
             Node temp = new Node(head, item);
             Node x = head;
-            while(x.next != null){
+            while (x.next != null) {
                 x = x.next;
             }
             x.next = temp;
@@ -26,10 +26,11 @@ public class LinkedQueue<T> implements Queue<T>{
     /**
      * Remove the item from the Queue at the front end.
      * Since we are not dealing with null data null is the same as empty
+     *
      * @return the item that is removed.
      */
-    public T poll(){
-        if (empty()){
+    public T poll() {
+        if (empty()) {
             return null;
         }
         T temp = head.data;
@@ -41,28 +42,27 @@ public class LinkedQueue<T> implements Queue<T>{
     /**
      * Peek at the first item at the front end.
      * Since we are not dealing with null data null is the same as empty
+     *
      * @return the first item at the front end, or null if this Queue is empty.
      */
-    public T peek(){
-        if (empty()){
+    public T peek() {
+        if (empty()) {
             return null;
         }
         return head.data;
     }
 
     /**
-     *
      * @return the number of the element in the queue.
      */
-    public int size(){
+    public int size() {
         return count;
     }
 
     /**
-     *
      * @return true if this Queue is empty, false otherwise
      */
-    public boolean empty(){
+    public boolean empty() {
         return head == null;
     }
 
@@ -72,6 +72,7 @@ public class LinkedQueue<T> implements Queue<T>{
 
         /**
          * constructor
+         *
          * @param next
          * @param data
          */
