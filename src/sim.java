0fc59e663f54dec42bce9e61d58d39dcef5c1710
Ryan <rsgm123@gmail.com>
2015-04-24 23:55:13 -0500

    Added sim.java

---
+++ src/sim.java
@@ -0,0 +1,14 @@
+/**
+ * Ian Leyden
+ * Ryan Mirman
+ * <p/>
+ * The source is on github at https://github.com/Rsgm/hairy-octo-wallhack
+ * <p/>
+ * Copyright under the MIT license
+ * http://opensource.org/licenses/MIT
+ */
+public class sim {
+    public static void main(String[] args) {
+
+    }
+}
